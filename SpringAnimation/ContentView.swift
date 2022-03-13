b2e34b9dc335b22d44c4124e396ab59fee043dcd
Khushi Bhardwaj <khushi.bhardwaj@gatech.edu>
2022-03-13 18:37:19 -0400

    Initial Commit

---
+++ SpringAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpringAnimation
+//
+//  Created by  Khushi Bhardwaj on 3/13/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SpringAnimation/SpringAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpringAnimationApp.swift
+//  SpringAnimation
+//
+//  Created by  Khushi Bhardwaj on 3/13/22.
+//
+
+import SwiftUI
+
+@main
+struct SpringAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
